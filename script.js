8a0d196c7b2761a58dd5d7b4185de4b46afa4cab
Pilopsis <okaspokus@gmail.com>
2025-09-30 21:07:34 -0400

    modified:   about.html 	new file:   cart.html 	modified:   contact.html 	new file:   "images/Floral crotchet piece \360\237\214\272\360\237\214\274\342\234\250.jpeg" 	new file:   images/blanket.jpg 	new file:   images/brown hat.jpg 	new file:   images/charger climber.jpg 	new file:   images/pillow.jpg 	new file:   images/pure cloret.jpg 	new file:   images/sunflower hat.jpg 	modified:   index.html 	modified:   script.js 	modified:   shop.html 	modified:   style.css

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 //Making Add to cart functional
 
-const cartBt=document.getElementById('cart-btn');
-const addToCartBtns=document.querySelectorAll('.add-to-cart')
+//const cartBt=document.getElementById('cart-btn');
+//const addToCartBtns=document.querySelectorAll('.add-to-cart')
 
-let cart=[];
+//let cart=[];
 
-addToCartBtns.forEach((btn,index)=>{
-    btn.addEventListener('click',()=>{
-        cart.push(`Item ${index + 1}`);
-        alert('Added to cart! Total items:${cart.lengh}');
-    });
-});
-addToCartBtns.addEventListener('click',()=>{
-    alert(`Your cart: ${cart.join(', ') || 'is empty'}`);
-}); 
+//addToCartBtns.forEach((btn,index)=>{
+    //btn.addEventListener('click',()=>{
+        //cart.push(`Item ${index + 1}`);
+        //alert('Added to cart! Total items:${cart.lengh}');
+    //});
+//});
+//addToCartBtns.addEventListener('click',()=>{
+    //alert(`Your cart: ${cart.join(', ') || 'is empty'}`);
+//}); 
